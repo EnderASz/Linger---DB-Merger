e2ebedb442b34ce0213b408774a0d1b17730a51c
EnderASz <olek5188@gmail.com>
2022-01-24 19:21:20 +0100

    Split main into functions and add NOQA for console module imports

--- __main__.py
+++ __main__.py
@@ -1,40 +1,54 @@
 import json
+import pathlib
 import sys
 
 from dotenv import load_dotenv
 load_dotenv()
 
-from src.console import init_console
+from src.console import init_console    # NOQA: E402
 init_console()
-from src.console import console     # NOQA: E402
+from src.console import console         # NOQA: E402
 
 
-sys.argv.pop(0)
-if len(sys.argv) < 3 or len(db_paths := set(sys.argv[:-1])) < 2:
-    console.error("Too few parameters passed.")
-    console.hint(
-        "There are minimum two [bold](not duplicated)[/bold] input",
-        "and one output database paths required.")
-    console.exit()
-output_path = sys.argv.pop()
+def parse_args() -> tuple[set[str | pathlib.Path], str | pathlib.Path]:
+    sys.argv.pop(0)
+    if len(sys.argv) < 3 or len(db_paths := set(sys.argv[:-1])) < 2:
+        console.error("Too few parameters passed.")
+        console.hint(
+            "There are minimum two [bold](not duplicated)[/bold] input",
+            "and one output database paths required.")
+        console.exit()
+    output_path = sys.argv.pop()
+    return db_paths, output_path
+
+
+db_paths, output_path = parse_args()
 console.debug("Input paths:", repr(db_paths))
 console.debug("Output path:", repr(output_path))
 
 
-words = set()
-for path in db_paths:
+def load_db(path: str | pathlib.Path) -> set[tuple[str, str]]:
     try:
         db = json.load(open(path, encoding='utf-8'))
     except FileNotFoundError:
         console.warn("Database", repr(path), "not found. Skipping...")
-        continue
+        return None
     console.debug("Size of", repr(path), ":", len(db))
-    words |= {tuple([word['wordEn'], word['wordPl']]) for word in db}
+    return {tuple([word['wordEn'], word['wordPl']]) for word in db}
 
-out_db = list()
-for word in words:
-    out_db.append({'wordEn': word[0], 'wordPl': word[1]})
-out_db.sort(key=lambda x: x['wordPl'])
+
+words: set[tuple[str, str]] = set()
+for path in db_paths:
+    words |= load_db(path)
+
+
+def pack(data: set[tuple[str, str]]) -> list[dict[str, str]]:
+    out_db = [{'wordEn': word[0], 'wordPl': word[1]} for word in data]
+    out_db.sort(key=lambda x: x['wordPl'])
+    return out_db
+
+
+out_db = pack(words)
 console.debug("Output size:", len(out_db))
 console.debug("Output data:", out_db)
 
